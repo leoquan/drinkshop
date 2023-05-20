68cab2e7986ae6d37c06104a76b3359e01bf3016
hongquanov <hongquanov@gmail.com>
2023-05-20 08:16:31 +0700

    init project

---
+++ BLUMini/BLUMini/Controllers/HtmlController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BLUMini.Controllers
+{
+    public class HtmlController : Controller
+    {
+        [Route("AboutUs")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        [Route("Blog")]
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        [Route("BlogDetails")]
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+        [Route("BlogDetailsGallery")]
+        public IActionResult BlogDetailsGallery()
+        {
+            return View();
+        }
+        [Route("BlogDetailsVideo")]
+        public IActionResult BlogDetailsVideo()
+        {
+            return View();
+        }
+        [Route("BlogRightSidebar")]
+        public IActionResult BlogRightSidebar()
+        {
+            return View();
+        }
+        [Route("CartPage")]
+        public IActionResult CartPage()
+        {
+            return View();
+        }
+        [Route("Checkout")]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [Route("LoginRegister")]
+        public IActionResult LoginRegister()
+        {
+            return View();
+        }
+        [Route("MyAccount")]
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+        [Route("ProductDetails")]
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        [Route("Shop")]
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        [Route("ShopList")]
+        public IActionResult ShopList()
+        {
+            return View();
+        }
+        [Route("Testimonial")]
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+        [Route("WishList")]
+        public IActionResult WishList()
+        {
+            return View();
+        }
+    }
+}
---
+++ BLUMini/BLUMini/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BLUMini.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
